ccd114cd52fc3a404993fe0eb78cc2018214aff1
VABIII <tbengtson82@gmail.com>
2022-01-18 22:50:14 -0600

    router ep built

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,12 +1,15 @@
+const db = require('../../data/db-config.db')
+
+const getRecipeById = id => {
+    return db('recipes').where('recipe_id', id).first()
+
+}
 
 
 
 
 
-
-
-
-
+module.exports = getRecipeById
 
 
 
--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
-const Recipes = require('./recipes-model')
+const Recipe = require('./recipes-model')
 
 
-router.get('/', (req, res, next) => {
-    console.log('get recipe')
+router.get('/:recipe_id', (req, res, next) => {
+    const { id } = req.params
+    Recipe.getRecipeById(id)
+        .then(resource => {
+            res.status(200).json(resource)
+        })
+        .catch(next)
 })
 
 
