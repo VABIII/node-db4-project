6965506620895b295e5a14c5e79c7b7398045a58
VABIII <tbengtson82@gmail.com>
2022-01-19 03:01:24 -0600

    working on return data

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,8 +1,9 @@
-// const db = require('../../data/db-config.db')
+const db = require('../../data/db-config')
 
-const getRecipeById = recipe_id => {
-    // return db('recipes').where('recipe_id', id).first()
-    return Promise.resolve(`Awesome recipe with id of ${recipe_id}`)
+const getRecipeById = async recipe_id => {
+    const rows = await db('recipes AS r')
+        .where('recipe_id', recipe_id).first()
+    return rows
 }
 
 
