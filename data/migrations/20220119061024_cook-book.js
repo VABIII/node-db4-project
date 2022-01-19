b408770cfab68c5a8a92abe857f78a5c4e00da1e
VABIII <tbengtson82@gmail.com>
2022-01-19 00:58:11 -0600

    migration file built out and migrations run

--- data/migrations/20220119061024_cook-book.js
+++ data/migrations/20220119061024_cook-book.js
@@ -2,22 +2,46 @@
 exports.up = async function(knex) {
     await knex.schema
         .createTable('recipes', tbl => {
-            tbl.increments()
-            // tbl.string('')
+            tbl.increments('recipes_id')
+            tbl.string('recipe_name', 200).notNullable().unique()
         })
         .createTable('ingredients', tbl => {
-            tbl.increments()
-            // tbl.string('ingredients_id')
+            tbl.increments('ingredient_id')
+            tbl.string('ingredient_name', 200).notNullable().unique()
+            tbl.integer('ingredient_unit').notNullable()
         })
         .createTable('steps', tbl => {
-            tbl.increments()
-            // tbl.string('')
+            tbl.increments('step_id')
+            tbl.string('step_text', 200).notNullable()
+            tbl.integer('step_order').notNullable()
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('recipe_id')
+                .inTable('recipes')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+
         })
         .createTable('step_ingredients', tbl => {
-            tbl.increments()
-            // tbl.string('')
-        })
+            tbl.increments('step_ingredient_id')
+            tbl.float('quantity').notNullable()
+            tbl.integer('step_id')
+                .unsigned()
+                .notNullable()
+                .references('step_id')
+                .inTable('steps')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('ingredient_id')
+                .inTable('ingredients')
+                .onDelete('RESTRICT')
+                .onUpdate('RESTRICT')
 
+        })
 };
 
 exports.down = async function(knex) {
